04cd173aa0e453bd045a6fa45549530b810f2bc7
Martín Chamarro <martin.chamarro@gmail.com>
2017-08-27 15:32:15 +0200

    EDIT -More coin ids and conversions added

--- domain/crypto/coin_id.py
+++ domain/crypto/coin_id.py
@@ -4,3 +4,5 @@ from enum import Enum
 class CoinId(Enum):
     BITCOIN = "bitcoin"
     ETHEREUM = "ethereum"
+    BITCOIN_CASH = "bitcoin-cash"
+    LITECOIN = "litecoin"
--- domain/crypto/conversion.py
+++ domain/crypto/conversion.py
@@ -5,3 +5,6 @@ class Conversion(Enum):
     EUR = "EUR"
     USD = "USD"
     BTC = "BTC"
+    ETH = "ETH"
+    BTH = "BTH"
+    LTC = "LTC"
