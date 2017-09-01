a54604357268009ef5b8852ff914df69ce687007
Martín Chamarro <martin.chamarro@gmail.com>
2017-09-01 13:25:28 +0200

    EDIT -Change in the date format to remove zero-padding

--- formatter/date_formatter.py
+++ formatter/date_formatter.py
@@ -2,7 +2,7 @@ from formatter import Formatter
 
 
 class DateFormatter(Formatter):
-    _FORMAT = "%a, %d %b '%y"
+    _FORMAT = "%a, %-d %b '%y"
 
     def format(self, value):
         return value.strftime(self._FORMAT)
